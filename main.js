873ef98f3295835a1baff81754b17c01cb1c2bb7
Nicole Ng <shinyinn@usc.edu>
2021-04-03 00:09:48 -0700

    add fade and scroll

---
+++ main.js
@@ -0,0 +1,12 @@
+// fade effect from https://webdesign.tutsplus.com/tutorials/simple-fade-effect-on-scroll--cms-35166
+const checkpoint = 50;
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll <= checkpoint) {
+    opacity = 1 - currentScroll / checkpoint;
+  } else {
+    opacity = 0;
+  }
+  document.querySelector(".front").style.opacity = opacity;
+});
